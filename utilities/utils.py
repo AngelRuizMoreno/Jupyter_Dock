fca767b4fd93133c69c254f978541d46862e4fe5
AngelRuizMoreno <angel.j.ruiz.moreno@gmail.com>
2021-09-10 22:16:37 -0500

    Molecular Docking

---
+++ utilities/utils.py
@@ -0,0 +1,161 @@
+import py3Dmol
+from pymol import cmd
+from openbabel import pybel
+
+from rdkit import Chem
+from rdkit.Chem import AllChem,rdFMCS, Draw
+
+from pdbfixer import PDBFixer
+from simtk.openmm.app import PDBFile
+
+import MDAnalysis as mda
+from MDAnalysis.coordinates import PDB
+
+def getbox(protein='',prot_opts={'format':'','object':'prot'},ligand='',lig_opts={'format':'mol2','object':'lig'}, selection='sele', extending = 6.0, software='vina'):
+    
+    cmd.load(filename=protein,**prot_opts)
+    cmd.load(filename=ligand,**lig_opts)
+    
+    ([minX, minY, minZ],[maxX, maxY, maxZ]) = cmd.get_extent(selection)
+
+    minX = minX - float(extending)
+    minY = minY - float(extending)
+    minZ = minZ - float(extending)
+    maxX = maxX + float(extending)
+    maxY = maxY + float(extending)
+    maxZ = maxZ + float(extending)
+    
+    SizeX = maxX - minX
+    SizeY = maxY - minY
+    SizeZ = maxZ - minZ
+    CenterX =  (maxX + minX)/2
+    CenterY =  (maxY + minY)/2
+    CenterZ =  (maxZ + minZ)/2
+    
+    cmd.delete('all')
+    
+    if software == 'vina':
+        return {'center_x':CenterX,'center_y': CenterY, 'center_z': CenterZ},{'size_x':SizeX,'size_y': SizeY,'size_z': SizeZ}
+    elif software == 'ledock':
+        return {'minX':minX, 'maxX': maxX},{'minY':minY, 'maxY':maxY}, {'minZ':minZ,'maxZ':maxZ}
+    elif software == 'both':
+        return ({'center_x':CenterX,'center_y': CenterY, 'center_z': CenterZ},{'size_x':SizeX,'size_y': SizeY,'size_z': SizeZ}),({'minX':minX, 'maxX': maxX},{'minY':minY, 'maxY':maxY}, {'minZ':minZ,'maxZ':maxZ})
+    
+    else:
+        print('software options must be "vina", "ledock" or "both"')
+
+
+def fix_protein(filename='',AddHs_pH=7.4,output='',try_renumeberResidues=False):
+
+    fix = PDBFixer(filename=filename)
+    fix.findMissingResidues()
+    fix.findNonstandardResidues()
+    fix.replaceNonstandardResidues()
+    fix.removeHeterogens(True)
+    fix.findMissingAtoms()
+    fix.addMissingAtoms()
+    fix.addMissingHydrogens(AddHs_pH)
+    PDBFile.writeFile(fix.topology, fix.positions, open(output, 'w'))
+
+    if try_renumeberResidues == True:
+        original=mda.Universe(output)
+        from_fix=mda.Universe(output)
+
+        resNum=[res.resid for res in original.residues]
+        for idx,res in enumerate(from_fix.residues):
+            res.resid = resNum[idx]
+
+        save=PDB.PDBWriter(filename=output)
+        save.write(from_fix)
+        save.close()
+        
+
+def generate_ledock_file(receptor='pro.pdb',rmsd=1.0,x=[0,0],y=[0,0],z=[0,0], n_poses=10, l_list='ligand.list',out='dock.in'):
+    rmsd=str(rmsd)
+    x=[str(x) for x in x]
+    y=[str(y) for y in y]
+    z=[str(z) for z in z]
+    n_poses=str(n_poses)
+    file=[
+        'Receptor\n',
+        receptor + '\n\n',
+        'RMSD\n',
+        rmsd +'\n\n',
+        'Binding pocket\n',
+        x[0],' ',x[1],'\n',
+        y[0],' ',y[1],'\n',
+        z[0],' ',z[1],'\n\n',
+        'Number of binding poses\n',
+        n_poses + '\n\n',
+        'Ligands list\n',
+        l_list + '\n\n',
+        'END']
+    
+    with open(out,'w') as output:
+        for line in file:
+            output.write(line)
+    output.close()
+
+
+
+def DOKMolSupplier (file=None):
+    mols=[]
+    with open(file, 'r') as f:
+        doc=[line for line in f.readlines()]
+    doc=[line.replace(line.split()[2],line.split()[2].upper()) if 'ATOM' in line else line for line in doc]
+    
+    scores=[line.split()[-2] for line in doc if 'REMARK Cluster' in line]
+    poses=[line.split()[2] for line in doc if 'REMARK Cluster' in line]
+
+    start=[index for (index,p) in enumerate(doc) if 'REMARK Cluster' in p]
+    finish=[index-1 for (index,p) in enumerate(doc) if 'REMARK Cluster' in p]
+    finish.append(len(doc))
+
+    interval=list(zip(start,finish[1:]))
+    for num,i in enumerate(interval):
+        try:
+            block = ",".join(doc[i[0]:i[1]]).replace(',','')
+
+            m=pybel.readstring(format='pdb',string=block)
+            mols.append(m)
+        except Exception:
+            pass
+    return(mols)
+
+def dok_to_sdf (dok_file=None,out_file=None):
+    mols=DOKMolSupplier(dok_file)
+    out=pybel.Outputfile(filename=out_file,format='sdf',overwrite=True)
+    for pose in mols:
+        pose.addh()
+        out.write(pose)
+    out.close()
+
+
+def get_3D_view (receptor_file='',rec_opts={'format':'pdb'},docking_results='',refMol='',refMol_opts={'format':'mol2'},pose=None):
+
+    view = py3Dmol.view()
+    view.removeAllModels()
+    view.setViewStyle({'style':'outline','color':'black','width':0.1})
+
+    view.addModel(open(receptor_file,'r').read(),**rec_opts)
+    Prot=view.getModel()
+    Prot.setStyle({'cartoon':{'arrows':True, 'tubes':True, 'style':'oval', 'color':'white'}})
+    view.addSurface(py3Dmol.VDW,{'opacity':0.6,'color':'white'})
+
+    if refMol:
+        view.addModel(open(refMol,'r').read(),**refMol_opts)
+        ref_m = view.getModel()
+        ref_m.setStyle({},{'stick':{'colorscheme':'purpleCarbon','radius':0.2}})
+
+    if pose:
+        results=Chem.SDMolSupplier(docking_results)
+        for index in pose:    
+            p=Chem.MolToMolBlock(results[index])
+            print (results[index].GetProp('REMARK'))
+
+            view.addModel(p,'mol')
+            x = view.getModel()
+            x.setStyle({},{'stick':{'colorscheme':'cyanCarbon','radius':0.1}})
+
+    view.zoomTo()
+    view.show()
